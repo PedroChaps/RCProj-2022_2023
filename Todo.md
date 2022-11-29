d460f97316be9fd8655b5a8ac3f75f53a836b504
PedroChaps <pedro.chaparro@tecnico.ulisboa.pt>
2022-11-29 22:27:32 +0000

    rev; quit and exit implemented; struct game; process_start(); beautiful prints

--- Todo.md
+++ Todo.md
@@ -1,7 +1,13 @@
-# Perguntas
+# Perguntar
 
 [X] - "GSport". O que acontece se a porta do outro lado não estiver aberta?
 [X] - Podemos usar um buffer com tamanho grande para não nos preocuparmos com Mallocs (para UDP só. Para TCP usamos buffer vamos preenchendo, enviando, preenchendo)
 [X] - Em UDP, se enviarmos uma letra mas o SV não responder, é suposto enviarmos denovo? Temos tipo um timeout que vai enviando?
 [X] - Temos que ir sempre pondo os comandos "SNG", "RSG", "PLG", ...
 [ ] - Podemos assumir que o input vem sempre bem formado? As in, não nos temos que preocupar com espaços a mais, validar que um IP é mesmo um IP, o PLID está entre ... e ..., etc.
+
+# TODOs 
+
+[ ] - Fazer download do enunciado denovo
+[ ] - Quando cliente faz "Create new game" e o sv não responde, temos que ter um timer que faz isso mais uma vez ou outra. Se ao fim dessas vezes o servidor não responder, informamos que o sv não respondeu
+[ ] - Ver Slides fixes 23-31... aula 3. Explicam servidores TCP concorrentes
--- client/player.c
+++ client/player.c
@@ -10,6 +10,25 @@
 
 #define GN 31
 #define PLID "099298"
+#define MAX_WORD_SIZE
+
+/* Printing information to the user ------------------------------------------------------------------ */
+#define GENERIC_ERROR "An error has occurred :(\nPlease try again!\n"
+#define WELCOME_MSG "Welcome to the 'RC Word Game'! :D\nPlease start a new game by typing "\
+                    "`start PLID`, where PLID is your student number.\n"
+#define ONGOING_GAME "There is still an ongoing game. Please continue it (`play *letter*`) or `quit` instead\n"
+#define STARTED_GAME "A new game has started! The word has >%d< letters and you have a maximum of >%d< errors!\n"
+#define AVAILABLE_COMMANDS "The following commands are available:\n"\
+"-`play *letter*` or `pl *letter*` - plays a letter;\n"\
+"-`guess *word*` or `gw *word*`    - guesses a word;\n"\
+"-`scoreboard` or `sb`             - Displays the scoreboard;\n"\
+"-`hint` or `h`                    - recieves a hint to help find the word;\n"\
+"-`state` or `st`                  - recieves a summary of the state of the current game or the most recent game;\n"\
+"-`quit`                           - quits the current game;\n"\
+"-`exit`                           - quits the current game and exists the program.\n"\
+
+
+/* --------------------------------------------------------------------------------------------------- */
 
 #define ERROR (-1)
 #define START 1
@@ -20,79 +39,117 @@
 #define STATE 6
 #define QUIT 7
 #define EXIT 8
+#define REV 9 // FIXME remover mais tarde
+
+int trials = 1;
+char plid[] = "000000";
+typedef struct game {
+    int curr_trial;
+    int max_errors;
+    int curr_errors;
+    int nr_letters;
+    char *current_word;
+} game;
+
+void print_current_word(game *game){
+    printf("The current word is:\n%s\n\n", game->current_word);
+}
+
+/* Initializes some variables of the current game and informs the user */
+int process_start(game *current_game, char *response){
+
+    // Separates the response code
+    char *splitted = strtok(response, " ");
+
+    // The response code "RSG" is expected from the server
+    if (strcmp(splitted, "RSG") != 0) {
+        printf(GENERIC_ERROR);
+        return -1;
+    }
+
+    // Separates the status code
+    splitted = strtok(NULL, " ");
+
+    // Processes the "NOK" status
+    if (strcmp(splitted, "NOK") == 0) {
+        printf(ONGOING_GAME);
+        return -1;
+    } // Checks for other status
+    else if (strcmp(splitted, "OK") != 0) {
+        printf(GENERIC_ERROR);
+        return -1;
+    }
+    // Processes the "OK" Status, that is, a new game was created
+    // Separates the number of letters and the maximum number of errors
+    splitted = strtok(NULL, " ");
+    current_game->nr_letters = atoi(splitted);
+
+    splitted = strtok(NULL, " ");
+    current_game->max_errors = atoi(splitted);
+    printf(STARTED_GAME, current_game->nr_letters, current_game->max_errors);
+
+    // Initializes the other variables
+    current_game->curr_errors = 0;
+    current_game->curr_trial = 1;
+
+    current_game->current_word = (char *) malloc(sizeof (char) * current_game->nr_letters);
+    memset(current_game->current_word, '_', current_game->nr_letters);
+
+    print_current_word(current_game);
+    printf(AVAILABLE_COMMANDS);
+    return 0;
+}
 
 
 /* sends a message and saves the message sent in buffer*/
-int send_message_udp(char *ip, char* port, char* cmd, char* arg_cmd, char* buffer, int flag) {
-    
+int send_message_udp(char *ip, char* port, char* cmd, char* buffer) {
 
-    char* hostname;
     int fd, errcode;
     ssize_t n;
-    socklen_t addrlen; // Tamanho do endereço
+    socklen_t addrlen;
 
-    /*
-    hints - Estrutura que contém informações sobre o tipo de conexão que será estabelecida.
-            Podem-se considerar, literalmente, dicas para o sistema operacional sobre como
-            deve ser feita a conexão, de forma a facilitar a aquisição ou preencher dados.
-
-    res - Localização onde a função getaddrinfo() armazenará informações sobre o endereço.
-    */
     struct addrinfo hints, *res;
     struct sockaddr_in addr;
 
-    /* Cria um socket UDP (SOCK_DGRAM) para IPv4 (AF_INET).
-    É devolvido um descritor de ficheiro (fd) para onde se deve comunicar. */
     fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd == -1) {
-        exit(1);
+        return -1;
     }
 
-    /* Preenche a estrutura com 0s e depois atribui a informação já conhecida da ligação */
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;      // IPv4
     hints.ai_socktype = SOCK_DGRAM; // UDP socket
 
-    /* Busca informação do host "localhost", na porta especificada,
-    guardando a informação nas `hints` e na `res`. Caso o host seja um nome
-    e não um endereço IP (como é o caso), efetua um DNS Lookup. */
     errcode = getaddrinfo(ip, port, &hints, &res);
     if (errcode != 0) {
-        exit(1);
+        return -1;
     }
 
-
-    /* Envia para o `fd` (socket) a mensagem "Hello!\n" com o tamanho 7.
-       Não são passadas flags (0), e é passado o endereço de destino.
-       É apenas aqui criada a ligação ao servidor. */
-    n = sendto(fd, "SNG 099298\n", 11, 0, res->ai_addr, res->ai_addrlen);
+    n = sendto(fd, cmd, strlen(cmd), 0, res->ai_addr, res->ai_addrlen);
     if (n == -1) {
-        exit(1);
+        return -1;
     }
 
-    /* Recebe 128 Bytes do servidor e guarda-os no buffer.
-       As variáveis `addr` e `addrlen` não são usadas pois não foram inicializadas. */
     addrlen = sizeof(addr);
     n = recvfrom(fd, buffer, 128, 0, (struct sockaddr *)&addr, &addrlen);
     if (n == -1) {
-        exit(1);
+        return -1;
     }
 
-    /* Imprime a mensagem "echo" e o conteúdo do buffer (ou seja, o que foi recebido
-    do servidor) para o STDOUT (fd = 1) */
     write(1, "echo: ", 6);
     write(1, buffer, n);
 
-    /* Desaloca a memória da estrutura `res` e fecha o socket */
     freeaddrinfo(res);
     close(fd);
-}
 
+    return 0;
+}
 
 
 void printUsage() {
     printf("Usage: ./player [-n GSIP] [-p GSport]");
 }
+
 
 void auxPrintArray(char **array) {
     int size = sizeof(array);
@@ -104,6 +161,7 @@ void auxPrintArray(char **array) {
     }
     printf("]");
 }
+
 
 /* Processes the input from the command line, attributing a new IP and a new Port if those arguments were passed */
 int processInput(int argc, char *argv[], char *ip, char *port){
@@ -121,71 +179,140 @@ int processInput(int argc, char *argv[], char *ip, char *port){
     return 1;
 }
 
-/* Reads a command and it's argument (if applicable) from the user */
-int readCommand(int *cmd, char *cmdArg){
 
-    int res, toReadArg = 0;
-    char cmdString[11]; // Creates a temporary buffer to read the command
+/* Reads a command and it's argument (if applicable) from the user */
+int readCommand(int *cmdCode, char *cmd){
+
+    int res;
+    char buffer[11]; // Creates a temporary buffer to read the command
 
     // Reads the command
-    res = scanf("%s", cmdString);
+    res = scanf("%s", buffer);
     if (res == -1)
         return -1;
 
-    // Maps the command to an integer and reads an argument (if applicable)
-    if (strcmp(cmdString, "start") == 0 || strcmp(cmdString, "sg") == 0){
-        *cmd = START;
-        toReadArg = 1;
-    }
+    // Maps the command to an integer and constructs the full command
 
-    else if (strcmp(cmdString, "play") == 0 || strcmp(cmdString, "pl") == 0){
-        *cmd = PLAY;
-        toReadArg = 1;
-    }
+    if (strcmp(buffer, "start") == 0 || strcmp(buffer, "sg") == 0){
 
-    else if (strcmp(cmdString, "guess") == 0 || strcmp(cmdString, "gw") == 0){
-        *cmd = GUESS;
-        toReadArg = 1;
-    }
+        *cmdCode = START;
 
-    else if (strcmp(cmdString, "scoreboard") == 0 || strcmp(cmdString, "sb") == 0)
-        *cmd = SCOREBOARD;
-
-    else if (strcmp(cmdString, "hint") == 0 || strcmp(cmdString, "h") == 0)
-        *cmd = HINT;
-
-    else if (strcmp(cmdString, "state") == 0 || strcmp(cmdString, "st") == 0)
-        *cmd = STATE;
-
-    else if (strcmp(cmdString, "quit") == 0)
-        *cmd = QUIT;
-
-    else if (strcmp(cmdString, "exit") == 0)
-        *cmd = EXIT;
-
-    else
-        *cmd = ERROR;
-
-    // If it's necessary to read an argument, reads it
-    if (toReadArg){
-        res = scanf("%s", cmdArg);
+        // Reads the PLID and saves it
+        res = scanf("%s", buffer);
         if (res == -1)
             return -1;
+        strcpy(plid, buffer);
+
+        // Creates the SNG command in the format "SNG PLID"
+        strcpy(cmd, "SNG ");
+        strcat(cmd, buffer);
+        strcat(cmd, "\n");
     }
+
+    else if (strcmp(buffer, "play") == 0 || strcmp(buffer, "pl") == 0){
+
+        *cmdCode = PLAY;
+
+        // Reads the letter to be played
+        res = scanf("%s", buffer);
+        if (res == -1)
+            return -1;
+
+        // Creates the PLG command in the format "PLG PLID letter trial"
+        strcpy(cmd, "PLG ");
+        strcat(cmd, plid);
+        strcat(cmd, " ");
+        strcat(cmd, buffer);
+        strcat(cmd, " ");
+        sprintf(buffer, "%d", trials);
+        strcat(cmd, buffer);
+        strcat(cmd, "\n");
+    }
+
+    else if (strcmp(buffer, "guess") == 0 || strcmp(buffer, "gw") == 0){
+
+        *cmdCode = GUESS;
+
+        // Reads the word
+        res = scanf("%s", buffer);
+        if (res == -1)
+            return -1;
+
+        // Creates the PWG command in the format "PWG PLID word trial"
+        strcpy(cmd, "PWG ");
+        strcat(cmd, plid);
+        strcat(cmd, " ");
+        strcat(cmd, buffer);
+        strcat(cmd, " ");
+        sprintf(buffer, "%d", trials);
+        strcat(cmd, buffer);
+        strcat(cmd, "\n");
+    }
+
+    else if (strcmp(buffer, "scoreboard") == 0 || strcmp(buffer, "sb") == 0) {
+        *cmdCode = SCOREBOARD; // TODO comando TCP
+
+    }
+
+    else if (strcmp(buffer, "hint") == 0 || strcmp(buffer, "h") == 0) {
+        *cmdCode = HINT; // TODO comando TCP
+    }
+
+    else if (strcmp(buffer, "state") == 0 || strcmp(buffer, "st") == 0) {
+        *cmdCode = STATE; // TODO comando TCP
+    }
+
+    else if (strcmp(buffer, "quit") == 0) {
+
+        *cmdCode = QUIT;
+
+        // Creates the QUT command in the format "QUT PLID"
+        strcpy(cmd, "QUT ");
+        strcat(cmd, plid);
+        strcat(cmd, "\n");
+    }
+
+    else if (strcmp(buffer, "exit") == 0) {
+
+        *cmdCode = EXIT;
+
+        // Creates the QUT command in the format "QUT PLID"
+        strcpy(cmd, "QUT ");
+        strcat(cmd, plid);
+        strcat(cmd, "\n");
+    }
+
+    // FIXME remover mais tarde - comando REV para revelar a palavra
+    else if (strcmp(buffer, "rev") == 0) {
+
+        *cmdCode = REV;
+        strcpy(cmd, "REV ");
+        strcat(cmd, plid);
+        strcat(cmd, "\n");
+    }
+
+    else
+        *cmdCode = ERROR;
 
     return 1;
 }
-        
+
+
 int main(int argc, char *argv[]) {
     
     char* response = (char*) malloc(sizeof(char)*1024);
-    int res;
-    int cmd;                         // The Command that the user passes
-    char *cmdArg = malloc(31);      // The argument of the command, if it exists
-    char* port = "58011";           // porta do tejo. Depois, substituir por 58000+GN;
+    int res, toExit = 0;
+    int cmdCode;                                            // The Command that the user passes
+    char *cmd = (char*) malloc(sizeof(char)*1024);      // The argument of the command, if it exists
+    char* port = "58011";                                   // porta do tejo. Depois, substituir por 58000+GN;
 
     char *ip = malloc(sizeof("___.___.___.___"));
     strcpy(ip, "193.136.138.142"); // ip do tejo. Depois, substituir por 127.0.0.1.
+
+    /* Initializes the structure Game */
+    game *current_game = (game *) malloc(sizeof(game));
+    current_game->curr_trial = 1;
+    current_game->current_word = malloc(sizeof(char) * MAX_WORD_SIZE + 1);
 
     // reads the input from the command line and defines a new IP and port if they were passed as arguments
     res = processInput(argc, argv, ip, port);
@@ -194,23 +321,34 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    while (1) {
-        // reads the command and it's argument (if applicable), from the user and communicates with the server
-        readCommand(&cmd, cmdArg);
-        switch (cmd) {
+    // Welcomes the user
+    printf(WELCOME_MSG);
+    while (!toExit) {
+        // reads the command and it's argument (if applicable), from the user and communicates with the server.
+        readCommand(&cmdCode, cmd);
+
+        // For each case, sends the message to the server and processes the response
+        switch (cmdCode) {
 
             case START:
-                printf("Start! Arg: %s\n", cmdArg);
-                send_message_udp(ip, port, "SNG", cmdArg, response, 1);
-                printf("%s\n", response);
+                res = send_message_udp(ip, port, cmd, response);
+                if (res == -1) {
+                    return -1;
+                }
+                process_start(current_game, response);
                 break;
 
             case PLAY:
-                printf("Play! Arg: %s\n", cmdArg);
+                //printf("Play! Arg: %s", cmd);
+                send_message_udp(ip, port, cmd, response);
+                current_game->curr_trial++; // TODO se a resposta for INV, não incremento curr_trial
                 break;
 
             case GUESS:
-                printf("Guess! Arg: %s\n", cmdArg);
+                //printf("Guess! Arg: %s", cmd);
+                send_message_udp(ip, port, cmd, response);
+                current_game->curr_trial++;
+
                 break;
 
             case SCOREBOARD:
@@ -226,18 +364,28 @@ int main(int argc, char *argv[]) {
                 break;
 
             case QUIT:
-                printf("Quit!\n");
+                //printf("Quit!\n");
+                send_message_udp(ip, port, cmd, response);
                 break;
 
             case EXIT:
-                printf("Exit!\n");
+                //printf("Exit!\n");
+                send_message_udp(ip, port, cmd, response);
+                toExit = 1;
+                break;
+
+            case REV:     // FIXME remover mais tarde - comando REV para revelar a palavra
+                //printf("Rev!\n");
+                send_message_udp(ip, port, cmd, response);
                 break;
 
             default:
-                printf("ERROR!\n");
+                printf("ERROR (ISTO É MEU, NÃO DO SERVER)!");
                 break;
         }
     }
+
+    return 0;
 }
 
 
